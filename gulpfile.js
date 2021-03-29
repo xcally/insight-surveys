ac67b5e9eb2236ddec7924ef539e29952f6bfc11
Vito Macchia <vito.macchia@gmail.com>
2021-03-29 15:11:48 +0200

    update deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,122 @@
-var gulp = require('gulp');
-var del = require('del');
-var merge = require('merge-stream');
-var plugins = require('gulp-load-plugins')();
-var Server = require('karma').Server;
-var browserSync = require('browser-sync').create();
-var argv = require('yargs').argv;
+var gulp = require("gulp");
+var del = require("del");
+var merge = require("merge-stream");
+var plugins = require("gulp-load-plugins")();
+var Server = require("karma").Server;
+var browserSync = require("browser-sync").create();
+var argv = require("yargs").argv;
 
-gulp.task('clean', function (cb) {
-    return del(['tmp', 'dist'], cb);
+gulp.task("clean", function (cb) {
+  return del(["tmp", "dist"], cb);
 });
 
-gulp.task('build-css', ['clean'], function () {
-    return gulp.src('./styles/*')
-        .pipe(plugins.plumber({ errorHandler: onError }))
-        .pipe(plugins.sass())
-        .pipe(plugins.minifyCss())
-        .pipe(plugins.rename({
-            extname: '.min.css'
-        }))
-        .pipe(gulp.dest('./dist'));
+gulp.task("build-css", ["clean"], function () {
+  return gulp
+    .src("./styles/*")
+    .pipe(plugins.plumber({ errorHandler: onError }))
+    .pipe(plugins.sass())
+    .pipe(plugins.minifyCss())
+    .pipe(
+      plugins.rename({
+        extname: ".min.css",
+      })
+    )
+    .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('build-tmp', ['build-css'], function () {
-    var builderStream = buildTemp('src/builder/', 'mwFormBuilder');
-    var viewerStream = buildTemp('src/viewer/', 'mwFormViewer');
-    var utilsStream = buildTemp('src/utils/', 'mwFormUtils');
-    return merge(builderStream, viewerStream, utilsStream);
+gulp.task("build-tmp", ["build-css"], function () {
+  var builderStream = buildTemp("src/builder/", "mwFormBuilder");
+  var viewerStream = buildTemp("src/viewer/", "mwFormViewer");
+  var utilsStream = buildTemp("src/utils/", "mwFormUtils");
+  return merge(builderStream, viewerStream, utilsStream);
 });
 
-gulp.task('default', ['build-tmp'], function () {
-    var i18n = gulp.src('i18n/**/*.json').pipe(plugins.jsonminify()).pipe(gulp.dest('dist/i18n/'));
+gulp.task("default", ["build-tmp"], function () {
+  var i18n = gulp
+    .src("i18n/**/*.json")
+    .pipe(plugins.jsonminify())
+    .pipe(gulp.dest("dist/i18n/"));
 
-    var builderStream = buildModuleStream('form-builder', 'mwFormBuilder');
-    var viewerStream = buildModuleStream('form-viewer', 'mwFormViewer');
-    var utilsStream = buildModuleStream('form-utils', 'mwFormUtils');
-    return merge(builderStream, viewerStream, utilsStream, i18n);
+  var builderStream = buildModuleStream("form-builder", "mwFormBuilder");
+  var viewerStream = buildModuleStream("form-viewer", "mwFormViewer");
+  var utilsStream = buildModuleStream("form-utils", "mwFormUtils");
+  return merge(builderStream, viewerStream, utilsStream, i18n);
 });
 
-gulp.task('watch', function() {
-    return gulp.watch(['i18n/**/*.json','./src/**/*.html', './styles/*.*css', 'src/**/*.js'], ['default']);
+gulp.task("watch", function () {
+  return gulp.watch(
+    ["i18n/**/*.json", "./src/**/*.html", "./styles/*.*css", "src/**/*.js"],
+    ["default"]
+  );
 });
 
 function buildTemp(src, moduleName) {
+  var tmpDir = "tmp/" + moduleName;
 
-    var tmpDir = 'tmp/'+moduleName;
+  var copy = gulp.src(src + "**/*").pipe(gulp.dest(tmpDir));
 
-    var copy = gulp.src(src + '**/*').pipe(gulp.dest(tmpDir));
-
-
-
-    return merge(copy);
+  return merge(copy);
 }
 
-function buildTemplates(src, moduleName, dest, filePrefix){
-    return gulp.src(src + '**/*.html')
-        .pipe(plugins.minifyHtml())
-        .pipe(plugins.angularTemplatecache({
-            module: moduleName,
-            filename: filePrefix+'-tpls.min.js'
-        }))
-        .pipe(plugins.uglify())
-        .pipe(gulp.dest(dest));
+function buildTemplates(src, moduleName, dest, filePrefix) {
+  return gulp
+    .src(src + "**/*.html")
+    .pipe(plugins.minifyHtml())
+    .pipe(
+      plugins.angularTemplatecache({
+        module: moduleName,
+        filename: filePrefix + "-tpls.min.js",
+      })
+    )
+    .pipe(plugins.uglify())
+    .pipe(gulp.dest(dest));
 }
 
 function buildModuleStream(destPrefix, moduleName) {
+  var tmpDir = "tmp/" + moduleName;
 
-    var tmpDir = 'tmp/'+moduleName;
+  var bootstrapTemplates = buildTemplates(
+    tmpDir + "/templates/bootstrap/",
+    moduleName,
+    "dist",
+    destPrefix + "-bootstrap"
+  );
+  var materialTemplates = buildTemplates(
+    tmpDir + "/templates/material/",
+    moduleName,
+    "dist",
+    destPrefix + "-material"
+  );
 
-    var bootstrapTemplates = buildTemplates(tmpDir+'/templates/bootstrap/', moduleName, 'dist', destPrefix+'-bootstrap');
-    var materialTemplates = buildTemplates(tmpDir+'/templates/material/', moduleName, 'dist', destPrefix+'-material');
+  var module = gulp
+    .src(tmpDir + "/**/*.js")
+    .pipe(plugins.plumber({ errorHandler: onError }))
+    .pipe(plugins.angularFilesort())
+    .pipe(plugins.ngAnnotate())
+    .pipe(plugins.concat(destPrefix + ".js"))
+    .pipe(gulp.dest("dist"));
+  var development = argv.dev === undefined ? false : true;
+  if (!development) {
+    module
+      .pipe(plugins.uglify())
+      .pipe(plugins.stripDebug())
+      .pipe(plugins.concat(destPrefix + ".min.js"))
+      .pipe(gulp.dest("dist"));
+  }
 
-    var module =  gulp.src(tmpDir + '/**/*.js')
-        .pipe(plugins.plumber({ errorHandler: onError }))
-        .pipe(plugins.angularFilesort())
-        .pipe(plugins.ngAnnotate())
-        .pipe(plugins.concat(destPrefix+'.js'))
-        .pipe(gulp.dest('dist'));
-    var development = (argv.dev === undefined) ? false : true;
-    if(!development){
-        module.pipe(plugins.uglify())
-            .pipe(plugins.stripDebug())
-            .pipe(plugins.concat(destPrefix+'.min.js'))
-            .pipe(gulp.dest('dist'));
-    }
-
-
-
-    return merge(module, bootstrapTemplates, materialTemplates);
+  return merge(module, bootstrapTemplates, materialTemplates);
 }
 
-gulp.task('test', function (done) {
-    new Server({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true
-    }, function() {
-        done();
-    }).start();
+gulp.task("test", function (done) {
+  new Server(
+    {
+      configFile: __dirname + "/karma.conf.js",
+      singleRun: true,
+    },
+    function () {
+      done();
+    }
+  ).start();
 });
 
 /**
@@ -105,40 +126,44 @@ gulp.task('test', function (done) {
 // error function for plumber
 var onError = function (err) {
   console.log(err);
-  this.emit('end');
+  this.emit("end");
 };
 
-
-var browserSyncInit = function(baseDir){
-    browserSync.init({
-        server: {
-            baseDir: baseDir,
-            index: "demo.html",
-            routes: {
-                "/bower_components": "bower_components",
-                "/vendor": "vendor",
-                "/dist": "dist",
-                "/i18n": "i18n"
-            }
-        },
-        port: 8080,
-        open: 'local',
-        browser: "google chrome"
-    });
+var browserSyncInit = function (baseDir) {
+  browserSync.init({
+    server: {
+      baseDir: baseDir,
+      index: "demo.html",
+      routes: {
+        "/node_modules": "node_modules",
+        "/vendor": "vendor",
+        "/dist": "dist",
+        "/i18n": "i18n",
+      },
+    },
+    port: 8080,
+    open: "local",
+    browser: "google chrome",
+  });
 };
 
-var gulpWatch = function(){
-    gulp.watch(['i18n/**/*.json', './src/**/*.html', './styles/*.*css', 'src/**/*.js'], ['default-watch']);
+var gulpWatch = function () {
+  gulp.watch(
+    ["i18n/**/*.json", "./src/**/*.html", "./styles/*.*css", "src/**/*.js"],
+    ["default-watch"]
+  );
 };
 
-gulp.task('default-watch', ['default'], ()=>{ browserSync.reload() });
-
-gulp.task('serve', ['default'], ()=>{
-    browserSyncInit("demo-material");
-    gulpWatch();
+gulp.task("default-watch", ["default"], () => {
+  browserSync.reload();
 });
 
-gulp.task('serve-bootstrap', ['default'], ()=>{
-    browserSyncInit("demo");
-gulpWatch();
+gulp.task("serve", ["default"], () => {
+  browserSyncInit("demo-material");
+  gulpWatch();
+});
+
+gulp.task("serve-bootstrap", ["default"], () => {
+  browserSyncInit("demo");
+  gulpWatch();
 });
--- karma.conf.js
+++ karma.conf.js
@@ -1,36 +1,31 @@
-module.exports = function(config) {
-    config.set({
-        frameworks: ['jasmine'],
-        plugins: [
-            'karma-phantomjs-launcher',
-            'karma-jasmine'
-        ],
-        files:[
-            'bower_components/jquery/dist/jquery.min.js',
-            'bower_components/jquery-ui/jquery-ui.min.js',
-            'bower_components/angular/angular.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            'bower_components/angular-bootstrap/ui-bootstrap.min.js',
-            'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
-            'bower_components/angular-sanitize/angular-sanitize.min.js',
-            'bower_components/angular-translate/angular-translate.min.js',
-            'bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js',
-            'bower_components/Sortable/Sortable.min.js',
-            'vendor/angular-legacy-sortable.js',
-            'bower_components/angular-elastic/elastic.js',
-            'dist/form-utils.js',
-            'dist/form-builder.min.js',
-            'dist/form-builder-bootstrap-tpls.min.js',
-            'dist/form-viewer.min.js',
-            'dist/form-viewer-bootstrap-tpls.min.js',
-            'test/*.js'
-
-
-        ],
-        // start these browsers
-        browsers: ['PhantomJS'],
-        reporters: ['progress'],
-        logLevel: config.LOG_INFO,
-        singleRun: false
-    });
+module.exports = function (config) {
+  config.set({
+    frameworks: ["jasmine"],
+    plugins: ["karma-phantomjs-launcher", "karma-jasmine"],
+    files: [
+      "node_modules/jquery/dist/jquery.min.js",
+      "node_modules/jquery-ui/jquery-ui.min.js",
+      "node_modules/angular/angular.js",
+      "node_modules/angular-mocks/angular-mocks.js",
+      "node_modules/angular-bootstrap/ui-bootstrap.min.js",
+      "node_modules/angular-bootstrap/ui-bootstrap-tpls.min.js",
+      "node_modules/angular-sanitize/angular-sanitize.min.js",
+      "node_modules/angular-translate/dist/angular-translate.min.js",
+      "node_modules/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js",
+      "node_modules/Sortable/Sortable.min.js",
+      "vendor/angular-legacy-sortable.js",
+      "node_modules/angular-elastic/elastic.js",
+      "dist/form-utils.js",
+      "dist/form-builder.min.js",
+      "dist/form-builder-bootstrap-tpls.min.js",
+      "dist/form-viewer.min.js",
+      "dist/form-viewer-bootstrap-tpls.min.js",
+      "test/*.js",
+    ],
+    // start these browsers
+    browsers: ["PhantomJS"],
+    reporters: ["progress"],
+    logLevel: config.LOG_INFO,
+    singleRun: false,
+  });
 };
